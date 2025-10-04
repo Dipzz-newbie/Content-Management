e353622b9b24b2b1516eb493fc8822b2b8469147
Dipzz-newbie <mnadhiftriyudo@gmail.com>
2025-10-04 15:11:40 +0700

    Update

--- FE/src/main.jsx
+++ FE/src/main.jsx
@@ -1,20 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {BrowserRouter, Route, Routes} from "react-router"
+import { BrowserRouter, Route, Routes } from "react-router"
 import Layout from './components/Layout'
 import UserRegister from './components/User/UserRegister'
 import UserLogin from './components/User/UserLogin'
+import DashboardLayout from './components/DashboardLayout'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route element={<Layout/>} >
-            <Route path='/register' element={<UserRegister/>}/>
-            <Route path='/login' element={<UserLogin/>}/>
+          <Route path='/register' element={<UserRegister />} />
+          <Route path='/login' element={<UserLogin />} />
         </Route>
-        <Route path='/dashboard'>
-
+        <Route path='/dashboard' element={<DashboardLayout/>}>
+          <Route path='contacts' element={<div>contacts</div>}/>
         </Route>
       </Routes>
     </BrowserRouter>
